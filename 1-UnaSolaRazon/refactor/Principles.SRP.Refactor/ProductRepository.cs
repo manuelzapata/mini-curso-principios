e0ccc63185c35ef7daed9f3728671e14599178e6
Manuel Ricardo Zapata <manuelricardozapata@gmail.com>
2018-07-26 23:49:52 -0500

    SRP refactor.

---
+++ 1-UnaSolaRazon/refactor/Principles.SRP.Refactor/ProductRepository.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Principles.SRP.Refactor
+{
+    class ProductRepository
+    {
+
+        private SqliteConnection masterConnection;
+
+        public ProductRepository() {
+            InitDatabase();
+        }
+
+        public void Save(Product newProduct) {
+            var insertCommand = masterConnection.CreateCommand();
+            insertCommand.CommandText = "INSERT INTO Product ( ProductId, Name, Price ) " +
+                                        "VALUES ( $id, $name, $price )";
+            insertCommand.Parameters.AddWithValue("$id", newProduct.ProductId);
+            insertCommand.Parameters.AddWithValue("$name", newProduct.Name);
+            insertCommand.Parameters.AddWithValue("$price", newProduct.Price);
+            insertCommand.ExecuteNonQuery();
+        }
+
+        public List<Product> List() {
+
+            var products = new List<Product>();
+
+            var selectCommand = masterConnection.CreateCommand();
+            selectCommand.CommandText = "SELECT ProductId, Name, Price FROM Product";
+            using (var reader = selectCommand.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    var product = new Product
+                    {
+                        ProductId = reader.GetInt32(0),
+                        Name = reader.GetString(1),
+                        Price = reader.GetDouble(2)
+                    };
+                    products.Add(product);
+                }
+            }
+
+            return products;
+        }
+
+        private void InitDatabase()
+        {
+
+            const string connectionString = "DataSource=Example;Mode=Memory;Cache=Shared";
+            masterConnection = new SqliteConnection(connectionString);
+            masterConnection.Open();
+
+            var createCommand = masterConnection.CreateCommand();
+            createCommand.CommandText =
+            @"
+                CREATE TABLE Product (
+                    ProductId INTEGER,
+                    Name TEXT,
+                    Price REAL
+                )
+            ";
+            createCommand.ExecuteNonQuery();
+        }
+
+
+    }
+}
---
+++ 1-UnaSolaRazon/refactor/Principles.SRP.Refactor/Service.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace Principles.SRP.Refactor
+{
+    class Service
+    {
+
+        private ProductRepository repository;
+
+        public Service() {
+            repository = new ProductRepository();
+        }
+
+        public double CalculateProductTax(Product product) {
+
+            //Validate product.
+            if (product == null) return 0;
+
+            const double Tax = 0.19;
+            double productTax = product.Price * Tax;
+            return productTax;
+        }
+
+        public bool SaveProduct(Product newProduct) {
+
+            //Validate product
+            if (newProduct == null || newProduct.ProductId < 0 || string.IsNullOrEmpty(newProduct.Name)) {
+                return false;
+            }
+
+            repository.Save(newProduct);
+            return true;
+        }
+
+        public List<Product> ListProducts() {
+
+            var products = repository.List();
+            return products;
+        }
+
+        
+
+    }
+}
